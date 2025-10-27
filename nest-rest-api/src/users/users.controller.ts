f6d3a60a5e3c32dd2250a9616bee9cd46ae73004
Wandonium <hillarywando@gmail.com>
2025-10-27 14:21:22 +0300

    nest-rest-api: Added user DTOs to user controller

--- nest-rest-api/src/users/users.controller.ts
+++ nest-rest-api/src/users/users.controller.ts
@@ -7,9 +7,11 @@ import {
   Patch,
   Post,
   Query,
-  ParseIntPipe
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -26,21 +28,16 @@ export class UsersController {
   }
 
   @Post() // POST /users
-  create(@Body() user: {
-    name: string,
-    email: string,
-    role: 'INTERN' | 'ENGINEER' | 'ADMIN'
-  }) {
-    return this.usersService.create(user);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: {
-    name: string,
-    email: string,
-    role: 'INTERN' | 'ENGINEER' | 'ADMIN'
-  }) {
-    return this.usersService.update(id, userUpdate);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id') // DELETE /users/:id
