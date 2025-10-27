6b5dcb1416a4a78e8302a5ca83a67d2857e6fbad
Wandonium <hillarywando@gmail.com>
2025-10-27 14:55:57 +0300

    nestjs-rest-api: Added the NotFoundException to the user service

--- nest-rest-api/src/users/users.service.ts
+++ nest-rest-api/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
@@ -46,6 +47,7 @@ export class UsersService {
 
   findOne(id: number) {
     const user = this.users.find((user) => user.id === id);
+    if (!user) throw new NotFoundException('User Not Found');
     return user;
   }
 
