6bf089edc8d61c665defeefe4b42ba8daed2aaae
Wandonium <hillarywando@gmail.com>
2025-10-27 14:09:28 +0300

    nestjs-rest-api: Added ParseIntPipe to user controller methods for int validation

--- nest-rest-api/src/users/users.controller.ts
+++ nest-rest-api/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Query,
+  ParseIntPipe
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 
@@ -20,8 +21,8 @@ export class UsersController {
   }
 
   @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
   @Post() // POST /users
@@ -34,16 +35,16 @@ export class UsersController {
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {
+  update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: {
     name: string,
     email: string,
     role: 'INTERN' | 'ENGINEER' | 'ADMIN'
   }) {
-    return this.usersService.update(+id, userUpdate);
+    return this.usersService.update(id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return this.usersService.delete(+id);
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
