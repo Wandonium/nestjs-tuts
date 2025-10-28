7111bc4112ca7562e866bfe43c76a956e9ad6734
Wandonium <hillarywando@gmail.com>
2025-10-28 15:53:56 +0300

    nestjs-rest-api: Added @prisma/client package. Created Employee crud resource.

--- nest-rest-api/prisma/database/database.service.ts
+++ nest-rest-api/prisma/database/database.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+  }
+}
