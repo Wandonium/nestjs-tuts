bf558a023063c5990911a318c738df084914c0cd
Wandonium <hillarywando@gmail.com>
2025-10-29 21:38:40 +0300

    nest-drizzle-app: Added db connection string to database module

--- nest-drizzle-app/src/database/database.module.ts
+++ nest-drizzle-app/src/database/database.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { DATABASE_CONNECTION } from './database-connection';
+import { ConfigService } from '@nestjs/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        });
+        return drizzle(pool, {
+          schema: {},
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
 export class DatabaseModule {}
