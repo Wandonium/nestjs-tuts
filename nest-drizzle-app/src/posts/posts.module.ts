164478db735aa7858c1faa75336d6b8bbc47c4f0
Wandonium <hillarywando@gmail.com>
2025-10-29 22:34:06 +0300

    nest-drizzle-app: Added one-to-many relationship to posts and users schemas

--- nest-drizzle-app/src/posts/posts.module.ts
+++ nest-drizzle-app/src/posts/posts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService],
 })
 export class PostsModule {}
--- nest-drizzle-app/src/users/schema.ts
+++ nest-drizzle-app/src/users/schema.ts
@@ -1,7 +1,13 @@
+import { relations } from 'drizzle-orm';
 import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { posts } from 'src/posts/schema';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   email: text('email').unique(),
   password: text('password'),
 });
+
+export const userRelations = relations(users, ({ many }) => ({
+  posts: many(posts),
+}));
