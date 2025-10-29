053b8e1526eb6ba009fa035a4a34ae7db36f7012
Wandonium <hillarywando@gmail.com>
2025-10-29 10:37:29 +0300

    nest-auth-app: Created new user entity

--- nest-auth-app/src/app.module.ts
+++ nest-auth-app/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'todo_user',
+      password: 'todo_password',
+      database: 'todo_db',
+      entities: ['**/entity/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
