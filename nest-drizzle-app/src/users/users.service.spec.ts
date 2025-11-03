7ddb24358f7a2ddb708e7edc13ddea0b8cf6c8d4
Wandonium <hillarywando@gmail.com>
2025-11-03 21:10:50 +0300

    nest-drizzle-app: Added DB connection to fix users.service test file

--- nest-drizzle-app/src/users/users.service.spec.ts
+++ nest-drizzle-app/src/users/users.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { DATABASE_CONNECTION } from '../database/database-connection';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: DATABASE_CONNECTION,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
