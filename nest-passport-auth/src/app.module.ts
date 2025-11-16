0aa61b6774de32189fe630e0dcbc90524270f796
Wandonium <hillarywando@gmail.com>
2025-11-16 21:58:11 +0300

    nest-passport-auth: Added global guards to protect all routes using Jwt strategy and expose public routes using public decorator.

--- nest-passport-auth/src/app.module.ts
+++ nest-passport-auth/src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- nest-passport-auth/src/auth/auth.controller.ts
+++ nest-passport-auth/src/auth/auth.controller.ts
@@ -2,20 +2,26 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Public } from './public-routes.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Get('test')
+  getTest(@Request() req) {
+    return 'testing 123';
   }
 }
