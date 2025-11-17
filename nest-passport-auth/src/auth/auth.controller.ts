5c5841cb8e9735c39e9a8a755716d4f1faeca3bb
Wandonium <hillarywando@gmail.com>
2025-11-17 09:56:57 +0300

    nest-passport-auth: Commit before adding refresh token implementation

--- nest-passport-auth/src/auth/auth.controller.ts
+++ nest-passport-auth/src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { Public } from './public-routes.decorator';
 
 @Controller('auth')
